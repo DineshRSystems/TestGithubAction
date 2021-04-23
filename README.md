c82c86ab6ba2c97318f0013e6b6fecebabd4b203
Dinesh Kumar <dinesh.kumar@rsystems.com>
2021-04-23 18:49:57 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# TestGithubAction
+# touchpoint-kit-ios
+
+## About
+- version: 0.0.9
+- min iOS version supported: 10.0
+
+###### Open the Terminal & use cd to switch to your  project directory. Initialise pod by running the following command.
+
+```
+pod init
+```
+
+###### This will create pod file. Add the following line below use_frameworks! in your pod file.
+
+```
+pod 'TouchPointKit', :git => 'https://github.com/vcilabs/touchpoint-kit-ios.git', :tag => '0.0.9'
+```
+
+###### Save it and then install the pod by running the following command.
+
+```
+pod install
+```
+
+###### This will pull the TouchPointKit framework from the Git repository. Finally, it creates a YOUR_PROJECT.xcworkspace file.  Then open YOUR_PROJECT.xcworkspace of  your project.
--- TouchPointKit.xcframework/ios-arm64_armv7/TouchPointKit.framework/Headers/TouchPointKit-Swift.h
+++ TouchPointKit.xcframework/ios-arm64_armv7/TouchPointKit.framework/Headers/TouchPointKit-Swift.h
@@ -224,6 +224,7 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) TouchPointAc
 @property (nonatomic) BOOL disableAllLogs;
 @property (nonatomic) BOOL disableCaching;
 @property (nonatomic) CGFloat defaultBannerHeight;
+@property (nonatomic) BOOL isStatusBarStyleLight;
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 /// Call this method on app launch to configure the TouchPoint SDK.
@@ -251,7 +252,7 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) TouchPointAc
 /// \param delegate (optional) Delegate to get the callback when TouchPoint activity
 /// completed so that you can hide your custom component
 ///
-- (void)openActivityWithScreenName:(NSString * _Nonnull)screenName delegate:(id <TouchPointActivityCompletionDelegate> _Nullable)delegate;
+- (void)openActivityForScreenWithScreenName:(NSString * _Nonnull)screenName delegate:(id <TouchPointActivityCompletionDelegate> _Nullable)delegate;
 /// Call this method to check if any TouchPoint activity pending to be
 /// displayed for current screen, if you want to open TouchPoint activity
 /// using custom component
@@ -261,7 +262,26 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) TouchPointAc
 /// returns:
 /// true if TouchPoint activity is not available or not seen by
 /// user for current screen
-- (BOOL)shouldShowActivityWithScreenName:(NSString * _Nonnull)screenName SWIFT_WARN_UNUSED_RESULT;
+- (BOOL)shouldShowActivityWithScreenName:(NSString * _Nonnull)screenName banner:(BOOL)banner SWIFT_WARN_UNUSED_RESULT;
+/// Open touchpoint activity by distribution url
+/// \param distUrl distribution url
+///
+/// \param useBannerStyling true for banner, false for custom component
+///
+/// \param delegate delegate
+///
+- (void)openActivityForUrlWithDistUrl:(NSString * _Nullable)distUrl useBannerStyling:(BOOL)useBannerStyling delegate:(id <TouchPointActivityCompletionDelegate> _Nullable)delegate alwaysShow:(BOOL)alwaysShow;
+/// Preload touchpoint activity for distribution url
+/// \param distUrl distribution url
+///
+- (void)cacheActivityForUrl:(NSString * _Nonnull)distUrl alwaysShow:(BOOL)alwaysShow;
+/// Check if an activity is seen by user or not
+/// \param distUrl distribution url
+///
+///
+/// returns:
+/// true if activity already seen else false
+- (BOOL)isActivitySeenWithDistUrl:(NSString * _Nullable)distUrl SWIFT_WARN_UNUSED_RESULT;
 @end
 
 
@@ -515,6 +535,7 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) TouchPointAc
 @property (nonatomic) BOOL disableAllLogs;
 @property (nonatomic) BOOL disableCaching;
 @property (nonatomic) CGFloat defaultBannerHeight;
+@property (nonatomic) BOOL isStatusBarStyleLight;
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 /// Call this method on app launch to configure the TouchPoint SDK.
@@ -542,7 +563,7 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) TouchPointAc
 /// \param delegate (optional) Delegate to get the callback when TouchPoint activity
 /// completed so that you can hide your custom component
 ///
-- (void)openActivityWithScreenName:(NSString * _Nonnull)screenName delegate:(id <TouchPointActivityCompletionDelegate> _Nullable)delegate;
+- (void)openActivityForScreenWithScreenName:(NSString * _Nonnull)screenName delegate:(id <TouchPointActivityCompletionDelegate> _Nullable)delegate;
 /// Call this method to check if any TouchPoint activity pending to be
 /// displayed for current screen, if you want to open TouchPoint activity
 /// using custom component
@@ -552,7 +573,26 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) TouchPointAc
 /// returns:
 /// true if TouchPoint activity is not available or not seen by
 /// user for current screen
-- (BOOL)shouldShowActivityWithScreenName:(NSString * _Nonnull)screenName SWIFT_WARN_UNUSED_RESULT;
+- (BOOL)shouldShowActivityWithScreenName:(NSString * _Nonnull)screenName banner:(BOOL)banner SWIFT_WARN_UNUSED_RESULT;
+/// Open touchpoint activity by distribution url
+/// \param distUrl distribution url
+///
+/// \param useBannerStyling true for banner, false for custom component
+///
+/// \param delegate delegate
+///
+- (void)openActivityForUrlWithDistUrl:(NSString * _Nullable)distUrl useBannerStyling:(BOOL)useBannerStyling delegate:(id <TouchPointActivityCompletionDelegate> _Nullable)delegate alwaysShow:(BOOL)alwaysShow;
+/// Preload touchpoint activity for distribution url
+/// \param distUrl distribution url
+///
+- (void)cacheActivityForUrl:(NSString * _Nonnull)distUrl alwaysShow:(BOOL)alwaysShow;
+/// Check if an activity is seen by user or not
+/// \param distUrl distribution url
+///
+///
+/// returns:
+/// true if activity already seen else false
+- (BOOL)isActivitySeenWithDistUrl:(NSString * _Nullable)distUrl SWIFT_WARN_UNUSED_RESULT;
 @end
 
 
